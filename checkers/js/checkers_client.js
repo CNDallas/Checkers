50f1ac4fbcc90f77befa3cf04e461a6da271ec56
Dallas Evers <cndallas@iastate.edu>
2018-11-07 16:23:53 -0600

    added react headers and footers to the appopriate pages

--- checkers/js/checkers_client.js
+++ checkers/js/checkers_client.js
@@ -1,8 +1,16 @@
+import react, {Component } from 'react';
+
+class checkers extends component {
+	render(){
+		//the webpage and what not
+	}
+}
+
 var socket = io('/checkers');
 
 function onCreate(){
 
 }
 
-
+export default checkers;
 
---
+++ login/index.js
@@ -0,0 +1,11 @@
+import react, {Component } from 'react';
+
+class index extends component {
+	render(){
+		//the php page should at some point be converted to javascript
+		//though this can be a secondary goal
+	}
+}
+
+
+export default index;
