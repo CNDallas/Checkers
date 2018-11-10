7210bb6c5656526552026f68110d362deee53091
darrona <darrona@iastate.edu>
2018-11-10 20:20:02 +0000

    Added capture and turns
    Working capture and turn note that its a very messy solution
    will be working to improve it

--- checkers/js/checkers_client.js
+++ checkers/js/checkers_client.js
@@ -1,17 +1,17 @@
-// import react, {Component } from 'react';
-//
-// class checkers extends component {
-// 	render(){
-// 		//the webpage and what not
-// 	}
-// }
-//
-// var socket = io('/checkers');
-//
-// export default checkers;
+import react, {Component } from 'react';
+
+class checkers extends component {
+	render(){
+		//the webpage and what not
+	}
+}
+
+var socket = io('/checkers');
+
+export default checkers;
 
 var selectedCell = null;
-
+var turn =0;
 function onCreate(){
   initBoard();
 }
@@ -25,9 +25,9 @@ function initBoard() {
     if ((Math.floor(i / 8) % 2) != (i % 2)) {
       cell.style.backgroundColor = "black";
       if (i < 24) {
-        cell.innerHTML = '<img src="img/p1_img.png"/>';
+        cell.innerHTML = '<img src="/img/p1_img.png"/>';
       } else if (i >= 40) {
-        cell.innerHTML = '<img src="img/p2_img.png"/>';
+        cell.innerHTML = '<img src="/img/p2_img.png"/>';
       }
     } else {
       cell.backgroundColor = "white";
@@ -37,13 +37,17 @@ function initBoard() {
 
 function selectCell(cell) {
   if (cell.style.backgroundColor == "black") {
-    if (selectedCell != null) {
-      if (selectedCell != cell&&isValid(selectedCell, cell)) {
-        doMove(selectedCell, cell);
-      }
+    if (selectedCell != null){
+		if((turn===0&& selectedCell.innerHTML.includes("p1_img"))||(turn===1)&&selectedCell.innerHTML.includes("p2_img")) {
+		
+			if (selectedCell != cell) {
+				doMove(selectedCell, cell);
+			}
+		}
       selectedCell.style.borderColor = "";
       selectedCell = null;
     }
+	
     cell.style.borderColor = "rgb(255, 0, 0)";
     selectedCell = cell;
   } else if (selectedCell != null) {
@@ -51,13 +55,52 @@ function selectCell(cell) {
       selectedCell = null;
     }
 }
-function isValid(origin,destination)
-{
-	return (destination.innerHTML == ""&&(Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===7||Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===9));
-}
+
 function doMove(origin, destination) {
-  if (destination.innerHTML == "") {
+	
+	if(destination.innerHTML !== "")
+	{
+			return;
+	}
+	
+  if (destination.innerHTML === ""&&(Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===7||Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===9)) {
     destination.innerHTML = origin.innerHTML;
     origin.innerHTML = "";
-  }
+	turn=Math.abs(turn-1); 
+	return;
+	}
+	var str 
+	str=destination.id.replace(destination.id.substr(4),parseInt(origin.id.substr(4))+(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))/2);
+	//str2=destination.id.replace(destination.id.substr(4),parseInt(origin.id.substr(4))-(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))/2);
+	 var i =document.getElementById(str);
+	 if(turn==0&&(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))===14&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("2"))
+	{
+		destination.innerHTML = origin.innerHTML;
+    document.getElementById(str).innerHTML="";
+		origin.innerHTML="";
+	turn=Math.abs(turn-1); 
+	}
+	else if(turn==1&&(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===14&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("1"))
+	{
+		destination.innerHTML = origin.innerHTML;
+    document.getElementById(str).innerHTML="";
+	origin.innerHTML="";
+	turn=Math.abs(turn-1); 
+	}
+	else if(turn==0&&(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))===18&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("2"))
+	{
+		destination.innerHTML = origin.innerHTML;
+    document.getElementById(str).innerHTML="";
+		origin.innerHTML="";
+	turn=Math.abs(turn-1); 
+	}
+	else if(turn==1&&(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===18&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("1"))
+	{
+		destination.innerHTML = origin.innerHTML;
+    document.getElementById(str).innerHTML="";
+	origin.innerHTML="";
+	turn=Math.abs(turn-1); 
+	}
+	var d =document.getElementById(str).innerHTML;
+	var t=Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)));
 }
