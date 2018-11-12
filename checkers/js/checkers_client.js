7e204755c591cd7a23226d54526e0693ac182988
Bailey Righi <brighi@iastate.edu>
2018-11-12 14:58:54 -0600

    made array for spaces, filled array with piece objects

--- checkers/js/checkers_client.js
+++ checkers/js/checkers_client.js
@@ -1,4 +1,4 @@
-import react, {Component } from 'react';
+/*import react, {Component } from 'react';
 
 class checkers extends component {
 	render(){
@@ -8,7 +8,9 @@ class checkers extends component {
 
 var socket = io('/checkers');
 
-export default checkers;
+export default checkers;*/
+
+var spaces = new Array(); //each element is either a piece object or null
 
 var selectedCell = null;
 var turn =0;
@@ -25,13 +27,16 @@ function initBoard() {
     if ((Math.floor(i / 8) % 2) != (i % 2)) {
       cell.style.backgroundColor = "black";
       if (i < 24) {
-        cell.innerHTML = '<img src="/img/p1_img.png"/>';
+				spaces[i] = /*piece : */{id: "piece" + i, isKing: false, isP1: true, posX: Math.floor(i / 8), posY: i % 8};
+        cell.innerHTML = '<img src="img/p1_img.png"/>';
       } else if (i >= 40) {
-        cell.innerHTML = '<img src="/img/p2_img.png"/>';
+				spaces[i] = /*piece : */{id: "piece" + i, isKing: false, isP1: false, posX: Math.floor(i / 8), posY: i % 8};
+        cell.innerHTML = '<img src="img/p2_img.png"/>';
       }
     } else {
       cell.backgroundColor = "white";
     }
+		console.log(spaces[i]);
   }
 }
 
@@ -39,7 +44,7 @@ function selectCell(cell) {
   if (cell.style.backgroundColor == "black") {
     if (selectedCell != null){
 		if((turn===0&& selectedCell.innerHTML.includes("p1_img"))||(turn===1)&&selectedCell.innerHTML.includes("p2_img")) {
-		
+
 			if (selectedCell != cell) {
 				doMove(selectedCell, cell);
 			}
@@ -47,7 +52,7 @@ function selectCell(cell) {
       selectedCell.style.borderColor = "";
       selectedCell = null;
     }
-	
+
     cell.style.borderColor = "rgb(255, 0, 0)";
     selectedCell = cell;
   } else if (selectedCell != null) {
@@ -57,19 +62,19 @@ function selectCell(cell) {
 }
 
 function doMove(origin, destination) {
-	
+
 	if(destination.innerHTML !== "")
 	{
 			return;
 	}
-	
+
   if (destination.innerHTML === ""&&(Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===7||Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===9)) {
     destination.innerHTML = origin.innerHTML;
     origin.innerHTML = "";
-	turn=Math.abs(turn-1); 
+	turn=Math.abs(turn-1);
 	return;
 	}
-	var str 
+	var str
 	str=destination.id.replace(destination.id.substr(4),parseInt(origin.id.substr(4))+(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))/2);
 	//str2=destination.id.replace(destination.id.substr(4),parseInt(origin.id.substr(4))-(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))/2);
 	 var i =document.getElementById(str);
@@ -78,28 +83,28 @@ function doMove(origin, destination) {
 		destination.innerHTML = origin.innerHTML;
     document.getElementById(str).innerHTML="";
 		origin.innerHTML="";
-	turn=Math.abs(turn-1); 
+	turn=Math.abs(turn-1);
 	}
 	else if(turn==1&&(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===14&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("1"))
 	{
 		destination.innerHTML = origin.innerHTML;
     document.getElementById(str).innerHTML="";
 	origin.innerHTML="";
-	turn=Math.abs(turn-1); 
+	turn=Math.abs(turn-1);
 	}
 	else if(turn==0&&(parseInt(destination.id.substr(4))-parseInt(origin.id.substr(4)))===18&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("2"))
 	{
 		destination.innerHTML = origin.innerHTML;
     document.getElementById(str).innerHTML="";
 		origin.innerHTML="";
-	turn=Math.abs(turn-1); 
+	turn=Math.abs(turn-1);
 	}
 	else if(turn==1&&(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)))===18&&document.getElementById(str).innerHTML!==""&&document.getElementById(str).innerHTML.includes("1"))
 	{
 		destination.innerHTML = origin.innerHTML;
     document.getElementById(str).innerHTML="";
 	origin.innerHTML="";
-	turn=Math.abs(turn-1); 
+	turn=Math.abs(turn-1);
 	}
 	var d =document.getElementById(str).innerHTML;
 	var t=Math.abs(parseInt(origin.id.substr(4))-parseInt(destination.id.substr(4)));
