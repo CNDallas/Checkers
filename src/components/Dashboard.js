cfa05b5510de842f9d3976c93f50ecee56afe2bb
cndallas <cndallas@iastate.edu>
2018-11-20 09:38:17 -0600

    Changed the OpenGameCard to a function instead of a class. There is no need for such to have any state properties

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {CREATE_GAME, JOIN_GAME, REFRESH_LOBBY, UPDATE_LOBBY} from '../api/Events'
+import {CREATE_GAME, IS_OPEN, JOIN_GAME, REFRESH_LOBBY, UPDATE_LOBBY} from '../api/Events';
 import OpenGameCard from './OpenGameCard'
 import "./css/Dashboard.css"
 import "tachyons"
@@ -9,52 +9,62 @@ import io from "socket.io-client"
 class Dashboard extends Component {
 
 	refreshLobby = () => {
-		const {socket} = this.props
+		const {socket} = this.props;
 		socket.emit(REFRESH_LOBBY, this.updateLobby)
-	}
+	};
 
 	updateLobby = (gameLobbies) => {
-		console.log(gameLobbies)
+		console.log(gameLobbies);
 		this.setState(gameLobbies)
-	}
+	};
 
 	createGame = () => {
-		const { socket } = this.props
+		const { socket } = this.props;
 		//const game = true;
 		socket.emit(CREATE_GAME, socket.username, (lobbyId) => {
 			this.props.moveToGame(lobbyId)
 		})
 
-	}
+	};
 
 	joinGame = (Id) => {
-		const { socket } = this.props
-		socket.emit(JOIN_GAME, Id)
+		const { socket } = this.props;
+		socket.emit(JOIN_GAME, Id);
 		this.props.moveToGame(Id)
-	}
+	};
 
 	joinLobby = () => {
-		const lobby = true
-		const socket  = io('/dashboard')
+		const lobby = true;
+		const socket  = io('/dashboard');
 		this.setState({lobby, socket})
-	}
+	};
+
+	joinGameHandler = (Id) => {
+		const {socket} = this.props;
+		console.log(Id);
+		socket.emit(IS_OPEN, Id, (isOpen) => {
+			if (isOpen) {
+				this.props.moveToGame(Id)
+			}
+		});
+	};
 
 	logout = () => {
 		//TODO
-	}
+	};
 
 	viewStats = () => {
 		//TODO
-	}
+	};
 
 	renderGames = (lobby) => {
-		const {socket} = this.props
+		const {socket} = this.props;
 		return <OpenGameCard socket = {socket} host={lobby.host}/>
-	}
+	};
 
 	constructor(props, context) {
-		super(props, context)
-		this.refreshLobby()
+		super(props, context);
+		this.refreshLobby();
 		this.state = {
 			gameLobbies: [],
 			createGame: false
@@ -63,12 +73,12 @@ class Dashboard extends Component {
 
 	render() {
 		const {gameLobbies} = this.state;
-		const {socket} = this.props
-		var cards = gameLobbies.length ===0?
+		const {socket} = this.props;
+		let cards = gameLobbies.length ===  0 ?
 			<h1>No games found. Why not create one</h1>:
 			gameLobbies.map(games =>
-				<OpenGameCard socket = {socket} host= {games.hostname} id={games.Id} moveToGame={this.joinGame}/>
-	)
+				<OpenGameCard socket = {socket} key={games.hostname} host={games.hostname} id={games.Id} joinGame={this.joinGameHandler.bind(this,games.Id)}/>
+	);
 
 		return (
 			<React.Fragment>
