5ee03ae174a12f2b15f0eb74be6bd981fb4ab383
cndallas <cndallas@iastate.edu>
2018-11-20 17:49:44 -0600

    Added some more back-end functions such as leaving a game. Currently if the server restarts while a client is visiting the website and then the client tries to do something, it will cause the server to crash. In later updates, once we parse data from the cookie and database, better authentication checks will be done.

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -3,20 +3,21 @@ import {AUTHENTICATE} from '../api/Events'
 import io from 'socket.io-client'
 import Dashboard from './Dashboard'
 import Checkers from './Checkers'
+import uuidv4 from 'uuid/v4'
 const socketUrl = "http://localhost:8081";
+
 
 class Controller extends Component {
 
 	initSocket = () => {
 		const socket = io(socketUrl);
-		const sessionID = "TestID"; //remove testing and make this work with the PHP cookie
-		const username = "TestUser"; //remove testing and make this work with the PHP cookie
+		const sessionID = uuidv4().substring(0,7); //remove testing and make this work with the PHP cookie
+		const username = "User" + uuidv4().substring(0,7); //remove testing and make this work with the PHP cookie
 		socket.on('connect', () => {
 			console.log("Connected")
 		});
-		socket.on(AUTHENTICATE, sessionID, username);
+		socket.emit(AUTHENTICATE, sessionID, username);
 		socket.username = username;
-		console.log(socket.username);
 		this.setState({socket})
 	};
 
--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -1,22 +1,29 @@
-import {AUTHENTICATE, CREATE_GAME,  USER_DISCONNECTED, LOGOUT, REFRESH_LOBBY, MAKE_MOVE, RECEIVE_MOVE, IS_GAME_OPEN, RECEIVE_MESSAGE, SEND_MESSAGE} from "../api/Events";
+import {AUTHENTICATE, CREATE_GAME, END_GAME, USER_DISCONNECTED, LOGOUT, REFRESH_LOBBY, MAKE_MOVE, RECEIVE_MOVE, IS_GAME_OPEN, RECEIVE_MESSAGE, SEND_MESSAGE} from "../api/Events";
 import uuidv4 from 'uuid/v4'
 
 const io = require('./index.js').io;
-
+const dashboard = "./dashboard";
 
 /* Handles communication with clients */
 module.exports = function (socket) {
-	var nsp = io.of('/dashboard');
 	console.log("User has joined");
-
 	//Account control listeners
 	socket.on(AUTHENTICATE, (sessionId, username) => {
-		if (checkConnectedUsers) {
-			//TODO redirect disconnected users to correct location
+		console.log("authenticating: ");
+		if (checkConnectedUsers(sessionId)) {
+			console.log("User is already connect with username: " + username + " and session ID: " + sessionId);
+			addConnectedUser(sessionId, username);
+			socket.username = username;
+			socket.join(dashboard);
+			socket.lobbyId = dashboard;
 		} else {
-			addConnectedUser(sessionId, username)
+			console.log("New user with username: " + username + " and session ID: " + sessionId);
+			addConnectedUser(sessionId, username);
+			socket.username = username;
+			socket.join(dashboard);
+			socket.lobbyId = dashboard;
 		}
-		console.log("authenticated");
+		console.log("authentication finished");
 	});
 
 	socket.on('disconnect', () => {
@@ -26,19 +33,24 @@ module.exports = function (socket) {
 
 	//Game Lobby listeners
 	socket.on(CREATE_GAME, (username, callback) => {
+		socket.isHost = true;
+		socket.leave(socket.lobbyId);
 		const lobbyId = uuidv4();
-		socket.join(lobbyId);
 		socket.lobbyId = lobbyId;
+		socket.join(lobbyId);
 		addGame(username, lobbyId);
-		console.log("Game Create: " + socket.lobbyId);
-		callback(lobbyId)
+		console.log("Game Created: " + socket.lobbyId);
+		callback(socket.lobbyId)
 	});
 
 	socket.on(IS_GAME_OPEN, (id, callback) => {
 		const open = isOpen(id);
-		socket.lobbyId = open ? joinGame(id):null;
-		socket.join(id);
-		console.log(socket.lobbyId + " isOpen: " + open);
+		if (open){
+			socket.lobbyId = joinGame(socket.username,id);
+			socket.join(socket.lobbyId);
+			socket.isHost = false;
+		}
+		console.log(id + " isOpen: " + open);
 		callback(open);
 		}
 	);
@@ -60,6 +72,15 @@ module.exports = function (socket) {
 		console.log("Message passed: " + message + " to lobby: " + socket.lobbyId);
 	});
 
+	socket.on(END_GAME, () => {
+		if (socket.isHost){
+			endGame(socket.lobbyId);
+		}
+		exitgameConnectedUsers(socket.username);
+		socket.lobbyId = dashboard;
+		socket.isHost = null;
+	});
+
 
 
 };
@@ -71,19 +92,23 @@ module.exports = function (socket) {
   immediately invoke at runtime */
 const gameCollection = new function () {
 	//TODO
-	this.totalgameCount = 0,
-		this.gameList = []
+	this.totalgameCount = 0;
+	this.gameList = []
 };
 
 function addGame(Creator, lobbyId) {
 	gameCollection.totalgameCount++;
 	gameCollection.gameList.push({Id:lobbyId, hostname: Creator, isOpen: true});
-	console.log(gameCollection.gameList.find(g => g.Id === lobbyId))
+	toGameConnectedUsers(Creator, lobbyId);
+	console.log(gameCollection.gameList.find(g => g.Id === lobbyId));
+	return lobbyId;
 }
 
-function joinGame(lobbyId) {
+function joinGame(user, lobbyId) {
 	console.log(lobbyId);
 	gameCollection.gameList.find(g => g.Id === lobbyId).isOpen = false;
+	gameCollection.gameList.find(g => g.Id === lobbyId).opponent = user;
+	toGameConnectedUsers(user, lobbyId);
 	return lobbyId;
 }
 
@@ -93,42 +118,68 @@ function isOpen(lobbyId){
 
 function endGame(lobbyId) {
 	gameCollection.totalgameCount--;
-	delete gameCollection.gameList[lobbyId]
+	delete gameCollection.gameList[lobbyId];
 }
 
 function getGames() {
-	return gameCollection.gameList.filter(g => g.isOpen !== false)
+	return gameCollection.gameList.filter(g => g.isOpen !== false);
+}
+
+function getAllGames(){
+	return gameCollection.gameList;
 }
 
 
-/*Collection of users in lobby
-  immediately invoke at runtime */
-const lobbyUsers = new function () {
+
+/*Collection of all logged in users with location*/
+let connectedUsers = new function () {
 	//TODO
-	this.totalgameCount = 0,
-		this.gameList = {}
-
-};
-
-
-/*Collection of all logged in users*/
-const connectedUsers = new function () {
-	//TODO
-	this.connectedUsersCount = 0,
-		this.userList = {
-			sessionID: {
-				userName: ""
-			}
-		}
+	this.userCount = 0;
+	this.lobbyCount = 0;
+	this.inGameCount = 0;
+	this.userList = [];
 };
 
 function checkConnectedUsers(sessionId) {
-	return sessionId in connectedUsers.userList
+	return connectedUsers.userList.find(user => user.Id === sessionId);
 }
 
-function addConnectedUser(sessionId, login) {
-	connectedUsers.connectedUsersCount++;
-	this.userList[sessionId].userName = login;
+function addConnectedUser(sessionId, userName) {
+	connectedUsers.userCount++;
+	connectedUsers.userList.push({Id: sessionId, username: userName, location: 'Unknown'});
+	console.log(connectedUsers.userList[connectedUsers.userList.length - 1]);
+	toDashboardConnectedUser(userName)//TODO may not want to automatically put the user into dashboard... be sure to change if such is decided
 }
 
+function toDashboardConnectedUser(userName){
+	connectedUsers.lobbyCount++;
+	connectedUsers.userList.find(user => user.username === userName).location = dashboard;
+}
+
+function toGameConnectedUsers(userName, lobbyId){
+	connectedUsers.lobbyCount--;
+	connectedUsers.inGameCount++;
+	console.log("Adding user: " + userName + " to lobby: " + lobbyId);
+	console.log(connectedUsers.userList.find(user => user.username === userName).username);
+	connectedUsers.userList.find(user => user.username === userName).location = lobbyId;
+}
+
+function exitgameConnectedUsers(userName){
+	connectedUsers.inGameCount--;
+	toDashboardConnectedUser(userName);
+}
+
+function logOutConnectedUser(sessionId){
+	connectedUsers.userCount--;
+	if ((connectedUsers.userList.find(user => user.Id === sessionId).location) === dashboard){
+		connectedUsers.lobbyCount--;
+	} else {
+		connectedUsers.inGameCount--;
+	}
+	delete connectedUsers.userList[sessionId];
+}
+
+function getUserList (){
+	return connectedUsers.userList;
+}
 
